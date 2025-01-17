bae2f6328e6d583dda2599240bccfb1aa07d9962
RigobertFrancis <RigobertFbuberwa@gmail.com>
2025-01-17 12:01:08 +0300

    Initialization of data in Constructor

--- ui/src/app/shared/components/patient-visit-history-modal/patient-visit-history-modal.component.ts
+++ ui/src/app/shared/components/patient-visit-history-modal/patient-visit-history-modal.component.ts
@@ -1,75 +1,48 @@
-import { Component, Inject, OnInit, OnDestroy } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { Store } from "@ngrx/store";
-import { Observable, of, Subscription } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { Observable } from "rxjs";
 import { AppState } from "src/app/store/reducers";
-import { getOpenMRSForms } from "src/app/store/selectors/form.selectors";
+import {
+  getAllForms,
+  getOpenMRSForms,
+} from "src/app/store/selectors/form.selectors";
 import { VisitsService } from "../../resources/visits/services";
-
-interface PatientVisitHistoryData {
-  patientUuid?: string;
-  shouldShowVitalsOnly?: boolean;
-  location?: {
-    tags?: { name: string }[];
-  };
-}
 
 @Component({
   selector: "app-patient-visit-history-modal",
   templateUrl: "./patient-visit-history-modal.component.html",
   styleUrls: ["./patient-visit-history-modal.component.scss"],
 })
-export class PatientVisitHistoryModalComponent implements OnInit, OnDestroy {
-  visits: any[] = [];
+export class PatientVisitHistoryModalComponent implements OnInit {
+  visits: any[];
   patientVisits$: Observable<any>;
-  patientUuid: string = "";
-  shouldShowVitalsOnly: Boolean = false;
+  patientUuid: string;
+  shouldShowVitalsOnly: Boolean;
   omitCurrent: boolean = true;
   forms$: Observable<any[]>;
-  private formsSubscription: Subscription;
-  private visitsSubscription: Subscription;
-
+  //.........Initialization of data in Constructor..............
   constructor(
     private visitService: VisitsService,
     private dialogRef: MatDialogRef<PatientVisitHistoryModalComponent>,
-    @Inject(MAT_DIALOG_DATA) private data: PatientVisitHistoryData,
+    @Inject(MAT_DIALOG_DATA) private data: any,
     private store: Store<AppState>
   ) {
-    this.patientUuid = data?.patientUuid || "";
+    this.patientUuid = data?.patientUuid || '';
     this.shouldShowVitalsOnly = !!(
       data?.shouldShowVitalsOnly ||
-      data?.location?.tags?.some(({ name }) => name === "Triage Location")
+      (data?.location?.tags?.find(({ name }) => name === 'Triage Location'))
     );
   }
+  
 
   ngOnInit(): void {
-    console.log("Injected Data:", this.data);
-
-    this.forms$ = this.store.select(getOpenMRSForms).pipe(
-      catchError((error) => {
-        console.error("Error fetching forms:", error);
-        return of([]);
-      })
+    this.forms$ = this.store.select(getOpenMRSForms);
+    this.patientVisits$ = this.visitService.getAllPatientVisits(
+      this.patientUuid,
+      true,
+      this.omitCurrent
     );
-
-    if (this.patientUuid) {
-      this.patientVisits$ = this.visitService
-        .getAllPatientVisits(this.patientUuid, true, this.omitCurrent)
-        .pipe(
-          catchError((error) => {
-            console.error("Error fetching patient visits:", error);
-            return of([]);
-          })
-        );
-    } else {
-      console.warn("No patient UUID provided.");
-      this.patientVisits$ = of([]);
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.formsSubscription?.unsubscribe();
-    this.visitsSubscription?.unsubscribe();
   }
 }
+
